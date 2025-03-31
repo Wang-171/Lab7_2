c34e97e3fd858a2971b5629fed81f01092fb2a3f
nguyendangQ <quangviplang@gmail.com>
2025-03-31 15:14:54 +0700

    update

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -9,7 +9,6 @@ public class Student {
         this.age = age;
     }
 
-
     public String getStudentId() { return studentId; }
     public String getName() { return name; }
     public int getAge() { return age; }
