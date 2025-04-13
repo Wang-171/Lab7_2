c7709665c2d2f58b4d14c84f7a1e40fa9ec28d7d
nguyendangQ <quangviplang@gmail.com>
2025-04-13 14:20:56 +0700

    update

---
+++ src/test/java/Test/UserTest.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test;
+
+/**
+ *
+ * @author quang
+ */
+import com.mycompany.userapp.User;
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+//    public void testUserConstructor() {
+//        User user = new User("user1", "user1@cmucs246.com");
+//        assertEquals("user1", user.getEmail());
+//    }
+
+    public void testSetEmail() {
+        User user = new User("user1", "user1@cmucs246.com");
+        user.setEmail("newemail@cmucs246.com");
+        assertEquals("newemail@cmucs246.com", user.getEmail());
+    }
+
+    public void testUpdateEmail() {
+        User user = new User("user1", "user1@cmucs246.com");
+        user.updateEmail("updatedemail@cmucs246.com");
+        assertEquals("updatedemail@cmucs246.com", user.getEmail());
+    }
+}
